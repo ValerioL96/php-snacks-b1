2112c347c43d7efcc8f304ac0db80ef8000ce291
ValerioL96 <v.lembo1396@gmail.com>
2024-06-26 12:23:51 +0200

    Created array

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+//Snack 1
+//Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario.
+
+$matches[
+//Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+[
+    "hometeam"=>"Acerrana",
+    "outteam"=>"Afragolana",
+    "homepoints"=>"10",
+    "homepoints"=>"2",
+],
+[
+    "hometeam"=>"Barillo",
+    "outteam"=>"Dolana",
+    "homepoints"=>"0",
+    "homepoints"=>"4",
+],
+[
+    "hometeam"=>"Fortino",
+    "outteam"=>"Perderello",
+    "homepoints"=>"2",
+    "homepoints"=>"8",
+],
+[
+    "hometeam"=>"Atim",
+    "outteam"=>"Sfonre",
+    "homepoints"=>"5",
+    "homepoints"=>"5",
+],
+]
+
+
+//Stampiamo a schermo tutte le partite con questo schema:
+//Olimpia Milano - Cantù | 55-60
+
+?>
