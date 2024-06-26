8daf4a9493268ef0a94f361d57c635634929fe81
ValerioL96 <v.lembo1396@gmail.com>
2024-06-26 18:39:11 +0200

    snack-6

---
+++ snack-6/index.php
@@ -0,0 +1,59 @@
+<?php
+
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+
+//Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./css/style.css">
+    
+    <title>Document</title>
+</head>
+<body>
+    <div class="bg-grey">
+        <ul>
+            <?php foreach($db as $teachers ){?>
+                <li>
+                    <?php echo $teachers['name'];?>  <?php echo $teachers['lastname'];?>
+                </li>
+            <?php }?>
+        </ul>
+    </div>
+    <div class="bg-green">
+        <ul>
+            <?php foreach($db as $pm ){?>
+                <li>
+                    <?php echo $pm['name'];?>  <?php echo $pm['lastname'];?>
+                </li>
+            <?php }?>
+        </ul>
+    </div>
+</body>
+</html>
