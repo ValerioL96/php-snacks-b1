9a39f875edb749cf1bb225c5bd64d8d57a70a6d5
ValerioL96 <v.lembo1396@gmail.com>
2024-06-27 12:59:53 +0200

    Added space in snack-8

--- snack-8/index.php
+++ snack-8/index.php
@@ -7,7 +7,7 @@ include __DIR__ .'/animals.php';
 <ul>
     <?php foreach($animals as $animal) {?>
         <li>
-                    <?php echo $animal['nome'];?>  <?php echo $animal['specie'];?> <?php echo $animal['classe'];?>
+                    <?php echo $animal['nome'];?> - <?php echo $animal['specie'];?> - <?php echo $animal['classe'];?>
                 </li>
             <?php }?>
 </ul>
