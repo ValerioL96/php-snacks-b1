00fcd8832ba327b85ae222e546729eb2e79b8e0a
ValerioL96 <v.lembo1396@gmail.com>
2024-06-27 12:57:59 +0200

    snack-8

---
+++ snack-8/animals.php
@@ -0,0 +1,30 @@
+<?php
+
+//Snack 8
+//Creare un array di animali, che abbiano nome, specie, classe in un file animals.php. 
+
+$animals=[
+    [
+        'nome'=>'delfino',
+        'specie'=>'mammifero',
+        'classe'=>'vertebrato',
+    ],
+    [
+        'nome'=>'serpente',
+        'specie'=>'rettile',
+        'classe'=>'invertebrato',
+    ],
+    [
+        'nome'=>'tigre',
+        'specie'=>'felino',
+        'classe'=>'vertebrato',
+    ],
+    [
+        'nome'=>'scorpione',
+        'specie'=>'aracnide',
+        'classe'=>'invertebrato',
+    ],
+]
+//Creo quattro array diversi in cui inserire soltanto mammiferi, pesci, rettili e tutti gli altri.
+//Nel nostro index.php stampiamo in pagina ognuno di questi array
+?>
---
+++ snack-8/index.php
@@ -0,0 +1,13 @@
+<?php
+
+include __DIR__ .'/animals.php';
+
+?>
+
+<ul>
+    <?php foreach($animals as $animal) {?>
+        <li>
+                    <?php echo $animal['nome'];?>  <?php echo $animal['specie'];?> <?php echo $animal['classe'];?>
+                </li>
+            <?php }?>
+</ul>
