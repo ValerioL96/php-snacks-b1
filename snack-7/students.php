0e3c182eb57675fbac12856a420d9d140dd45c24
ValerioL96 <v.lembo1396@gmail.com>
2024-06-27 12:41:50 +0200

    snack-7

---
+++ snack-7/students.php
@@ -0,0 +1,40 @@
+<?php
+
+//Snack 7
+//Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+//Questo array dovra' essere inserito in un file separato, da importare.
+//Stampare Nome, Cognome e la media dei voti di ogni alunno.
+
+include __DIR__ .'/votes';
+
+$students = [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Palo',
+            'votes'=> $votes,
+        ],
+        [
+            'name' => 'Gioele',
+            'lastname' => 'Pio',
+            'votes'=> $votes,
+        ],
+        [
+            'name' => 'Maria',
+            'lastname' => 'Filippi',
+            'votes'=> $votes,
+        ],
+        [
+            'name' => 'Rosella',
+            'lastname' => 'Diaz',
+            'votes'=> $votes,
+        ],
+];
+?>
+
+<ul>
+    <?php foreach($students as $student) {?>
+        <li>
+                    <?php echo $student['name'];?>  <?php echo $student['lastname'];?> <?php echo $student['votes'];?>
+                </li>
+            <?php }?>
+</ul>
---
+++ snack-7/votes.php
@@ -0,0 +1,26 @@
+<?php
+
+$votes=[
+
+    
+    [
+        'matematica'=>'8',
+        'italiano' =>'6',
+        'storia'=>'7',
+    ],
+    [
+        'matematica'=>'9',
+        'italiano' =>'10',
+        'storia'=>'9',
+    ],
+    [
+        'matematica'=>'5',
+        'italiano' =>'6',
+        'storia'=>'2',
+    ],
+    [
+        'matematica'=>'4',
+        'italiano' =>'5',
+        'storia'=>'7',
+    ],
+]
